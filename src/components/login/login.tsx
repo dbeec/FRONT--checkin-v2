24bf01533e580a56ed6031507e5efd095b5aa166
sershcodes <sergio.galvis@etfundacion.org>
2024-03-14 10:46:24 -0500

    login por la mitad

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,6 +1,42 @@
 import { TextField } from "@mui/material";
 import "./login.css";
+import React, { useState } from "react";
+import { apiBackend } from "../../config/config";
+import axios from "axios";
+
 export default function Login() {
+  const [auth, setAuth] = useState({
+    document: "",
+    empPass: "",
+  });
+
+  const handleInputOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setAuth({
+      ...auth,
+      [name]: value,
+    });
+  };
+
+  const handleLogin = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    axios
+      .post(`${apiBackend}/auth/login`, {
+        document: auth.document,
+        empPass: auth.empPass,
+      })
+      .then((response) => {
+        if (response.status === 201) {
+          const token = response.data.AccessToken;
+          localStorage.setItem("access_token", token);
+          alert("success");
+        }
+      })
+      .catch((error) => {
+        alert(error.response.data.message);
+        console.log(error);
+      });
+  };
   return (
     <>
       <div className="main">
@@ -13,8 +49,10 @@ export default function Login() {
           <div className="main__contentinputs">
             <TextField
               id="outlined-basic"
-              label="Email"
+              label="Document"
+              name="document"
               variant="outlined"
+              onChange={handleInputOnChange}
               size="small"
             />
           </div>
@@ -23,13 +61,16 @@ export default function Login() {
             <TextField
               id="outlined-basic"
               label="Password"
+              type="password"
+              name="empPass"
               variant="outlined"
+              onChange={handleInputOnChange}
               size="small"
             />
           </div>
 
           <div className="main__button">
-            <a href="/admin/wowdesarrollos">Enter</a>
+            <button onClick={handleLogin}>Enter</button>
           </div>
         </form>
       </div>
---
+++ src/config/config.ts
@@ -0,0 +1,1 @@
+export const apiBackend = import.meta.env.VITE_API_BACKEND
