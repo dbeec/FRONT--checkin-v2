2c2502ae6629d91254ee16052b2e8b0ee2a24370
Johan Díaz <johan.diaz@etfundacion.org>
2024-03-15 17:11:37 -0500

    updated

--- src/components/forms/create-users/create.users.tsx
+++ src/components/forms/create-users/create.users.tsx
@@ -1,70 +1,67 @@
-import { TextField } from "@mui/material";
+import {
+  FormControl,
+  MenuItem,
+  Select,
+  TextField,
+  SelectChangeEvent,
+} from "@mui/material";
+import InputLabel from "@mui/material/InputLabel";
 import "./create.users.css";
+import React from "react";
+
 export default function CreateNewUser() {
+  const [documentType, setDocumentType] = React.useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setDocumentType(event.target.value);
+  };
   return (
     <>
       <form className="form">
-        <h3>Users register</h3>
-        <div className="content-inputs">
-          <TextField
-            id="outlined-basic"
-            label="Document"
-            name="document"
-            variant="outlined"
-            size="small"
-            sx={{
-              "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline":
-                {
-                  borderColor: "#302c2c",
-                },
-            }}
-          />
+        <FormControl sx={{ minWidth: 120 }} size="small">
+          <InputLabel id="demo-select-small-label">Document type</InputLabel>
+          <Select
+            labelId="demo-select-small-label"
+            id="demo-select-small"
+            value={documentType}
+            label="Document Type"
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>CC</MenuItem>
+            <MenuItem value={20}>TI</MenuItem>
+            <MenuItem value={30}>CE</MenuItem>
+          </Select>
+        </FormControl>
 
-          <TextField
-            id="outlined-basic"
-            label="Document"
-            name="document"
-            variant="outlined"
-            size="small"
-            sx={{
-              "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline":
-                {
-                  borderColor: "#302c2c",
-                },
-            }}
-          />
+        <TextField
+          id="outlined-basic"
+          label="Document"
+          name="document"
+          variant="outlined"
+          size="small"
+          sx={{
+            "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+              borderColor: "#302c2c",
+            },
+          }}
+        />
 
-          <TextField
-            id="outlined-basic"
-            label="Document"
-            name="document"
-            variant="outlined"
-            size="small"
-            sx={{
-              "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline":
-                {
-                  borderColor: "#302c2c",
-                },
-            }}
-          />
-
-          <TextField
-            id="outlined-basic"
-            label="Document"
-            name="document"
-            variant="outlined"
-            size="small"
-            sx={{
-              "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline":
-                {
-                  borderColor: "#302c2c",
-                },
-            }}
-          />
-        </div>
+        <TextField
+          id="outlined-basic"
+          label="Name"
+          name="name"
+          variant="outlined"
+          size="small"
+          sx={{
+            "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+              borderColor: "#302c2c",
+            },
+            minWidth: 600,
+          }}
+        />
 
         <div className="button">
-          <button>Save</button>
+          <button>Create</button>
         </div>
       </form>
     </>
